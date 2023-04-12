711b9bc9cc69d1a82b3ee7bed0e9fe42467e724f
LesherG <LesherG@oregonstate.edu>
2023-04-11 18:55:39 -0700

    haha

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH -J Assignment 1 test						  # name of job
+#SBATCH -J Assignment_1_test						  # name of job
 #SBATCH -A cs479-579	  # name of my sponsored account, e.g. class or research group, NOT ONID!
+#SBATCH -p share								  # name of partition or queue
 #SBATCH -o A1t.out				  # name of output file for this submission script
 #SBATCH -e A1e.err				  # name of error file for this submission script
 
